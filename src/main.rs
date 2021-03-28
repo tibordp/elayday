e344d6497abf8232f1eae8560c749218f3c3fb82
Tibor Djurica Potpara <tibor.djurica@ojdip.net>
2021-03-28 14:39:35 +0100

    make clippy happy

--- src/main.rs
+++ src/main.rs
@@ -143,7 +143,8 @@ impl Elayday for ElaydayService {
         self.mailbox
             .clone()
             .send(ApiMessage::Put(request.key, request.value, tx))
-            .await;
+            .await
+            .unwrap();
 
         rx.await.unwrap();
 
@@ -159,7 +160,8 @@ impl Elayday for ElaydayService {
         self.mailbox
             .clone()
             .send(ApiMessage::Get(request.key, tx))
-            .await;
+            .await
+            .unwrap();
 
         Ok(Response::new(GetValueResponse {
             value: rx.await.unwrap(),
@@ -200,8 +202,8 @@ struct Processor {
 impl Processor {
     fn new(bind_address: SocketAddr, destination_address: SocketAddr) -> Self {
         Processor {
-            bind_address: bind_address,
-            destination_address: destination_address,
+            bind_address,
+            destination_address,
             state: ProcessorState::new(),
             mtu: 32767,
         }
@@ -244,7 +246,7 @@ impl Processor {
                                     val.value_id.unwrap().into(),
                                     GetClaim {
                                         fragments: vec![None; val.num_fragments as usize],
-                                        reply: reply,
+                                        reply,
                                     },
                                 );
                             }
@@ -270,7 +272,7 @@ impl Processor {
                                         .flat_map(|x| x.unwrap())
                                         .collect();
                                     println!("Ahoj!");
-                                    claim.reply.send(response);
+                                    claim.reply.send(response).unwrap();
                                 }
                             }
                         }
@@ -296,9 +298,9 @@ impl Processor {
                 outbox.push(Frame {
                     r#type: FrameType::Value as i32,
                     payload: Some(frame::Payload::Value(Value {
-                        key: key,
+                        key,
                         value_id: Some(value_id.into()),
-                        num_fragments: num_fragments,
+                        num_fragments,
                     })),
                 });
                 reply.send(()).unwrap();
@@ -311,7 +313,7 @@ impl Processor {
                 let ping_id = uuid::Uuid::new_v4();
                 self.state
                     .ping_lookup
-                    .insert(ping_id, (self.state.frame_count, Instant::now()));
+                    .insert(ping_id, (self.state.frame_count, instant));
 
                 outbox.push(Frame {
                     r#type: FrameType::Ping as i32,
@@ -341,15 +343,12 @@ impl Processor {
         pin_mut!(stream);
 
         loop {
-            match futures::stream::StreamExt::next(&mut stream).await {
-                Some(api_message) => {
-                    for frame in self.process_message(api_message) {
-                        //tokio::time::delay_for(Duration::from_millis(500)).await;
-                        udp_write.send((frame, self.destination_address)).await?;
-                    }
+            if let Some(api_message) = futures::stream::StreamExt::next(&mut stream).await {
+                for frame in self.process_message(api_message) {
+                    //tokio::time::delay_for(Duration::from_millis(500)).await;
+                    udp_write.send((frame, self.destination_address)).await?;
                 }
-                _ => {}
-            };
+            }
         }
     }
 }
@@ -385,7 +384,7 @@ async fn run_reflector(
     bind_address: SocketAddr,
     delay: std::time::Duration,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let (mut tx, mut rx) = unbounded_channel();
+    let (tx, mut rx) = unbounded_channel();
 
     let socket = UdpSocket::bind(bind_address).await?;
     let (mut udp_tx, mut udp_rx) =
@@ -400,8 +399,7 @@ async fn run_reflector(
 
     while let Some(Ok((buf, return_address))) = udp_rx.next().await {
         let execute_at = tokio::time::Instant::now() + delay;
-        tx.send((execute_at, buf.into(), return_address))
-            .unwrap();
+        tx.send((execute_at, buf.into(), return_address)).unwrap();
     }
     drop(tx);
 
@@ -415,7 +413,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches = App::new("elayday")
         .version("1.0")
         .author("Tibor Djurica Potpara <tibor.djurica@ojdip.net>")
-        .about("Does awesome things")
+        .about("A UDP delay line key-value datastore")
+        .setting(AppSettings::ArgRequiredElseHelp)
         .subcommand(
             App::new("server") // The name we call argument with
                 .about("Runs the server") // The message displayed in "myapp -h"
